aaaf8decc6fbac86a95f403434b12d68a67ffc18
Mobcn <mobingcn@gmail.com>
2023-03-18 15:22:49 +0800

    init

--- api/test.js
+++ api/test.js
@@ -1,45 +1,64 @@
+import { createParser } from 'eventsource-parser';
+
 /**
  * @param {VercelRequest} req Vercel请求对象
  * @param {VercelResponse} res Vercel响应对象
  */
-// export default async (req, res) => {
-//     const apiKey = req.query['key'];
-//     if (!apiKey) {
-//         res.send('没有apiKey！');
-//         return;
-//     }
-//     const initOptions = {
-//         headers: {
-//             'Content-Type': 'application/json',
-//             Authorization: `Bearer ${apiKey}`
-//         },
-//         method: 'POST',
-//         body: JSON.stringify({
-//             model: 'gpt-3.5-turbo',
-//             messages: [{ role: 'user', content: '你好' }],
-//             temperature: 0.7
-//         })
-//     };
-//     const rawResponse = await fetch('https://api.openai.com/v1/chat/completions', initOptions).catch((err) => {
-//         res.send(`code: ${err.name}, message: ${err.message}`);
-//     });
-//     if (rawResponse) {
-//         res.send(await rawResponse.text());
-//     } else {
-//         res.send('请求失败！');
-//     }
-// };
-
-export const post = async (context) => {
-    return new Response(
-        JSON.stringify({
-            data: {
-                code: 200,
-                message: '测试'
+export default async (req, res) => {
+    const apiKey = req.query['key'];
+    if (!apiKey) {
+        res.send('没有apiKey！');
+        return;
+    }
+    const initOptions = {
+        headers: {
+            'Content-Type': 'application/json',
+            Authorization: `Bearer ${apiKey}`
+        },
+        method: 'POST',
+        body: JSON.stringify({
+            model: 'gpt-3.5-turbo',
+            messages: [{ role: 'user', content: '你好' }],
+            temperature: 0.6,
+            stream: true
+        })
+    };
+    try {
+        /** @type {Response} */
+        const rawResponse = await fetch('https://api.openai.com/v1/chat/completions', initOptions);
+        if (!rawResponse.ok) {
+            res.send('请求失败！');
+        }
+        const encoder = new TextEncoder();
+        const decoder = new TextDecoder();
+        const stream = new ReadableStream({
+            async start(controller) {
+                const parser = createParser((event) => {
+                    if (event.type === 'event') {
+                        const data = event.data;
+                        if (data === '[DONE]') {
+                            controller.close();
+                            return;
+                        }
+                        try {
+                            const json = JSON.parse(data);
+                            const text = json.choices[0].delta?.content || '';
+                            const queue = encoder.encode(text);
+                            controller.enqueue(queue);
+                        } catch (e) {
+                            controller.error(e);
+                        }
+                    }
+                });
+                for await (const chunk of rawResponse.body) {
+                    parser.feed(decoder.decode(chunk));
+                }
             }
-        }),
-        { status: 200 }
-    );
+        });
+        res.send(stream);
+    } catch (err) {
+        res.send(`code: ${err.name}, message: ${err.message}`);
+    }
 };
 
 /**
