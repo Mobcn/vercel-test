41fcd5d42847658ae6a2ffe1685c1ddc67e9ac6b
Mobcn <mobingcn@gmail.com>
2023-03-18 15:08:35 +0800

    init

--- api/test.js
+++ api/test.js
@@ -2,32 +2,44 @@
  * @param {VercelRequest} req Vercel请求对象
  * @param {VercelResponse} res Vercel响应对象
  */
-export default async (req, res) => {
-    const apiKey = req.query['key'];
-    if (!apiKey) {
-        res.send('没有apiKey！');
-        return;
-    }
-    const initOptions = {
-        headers: {
-            'Content-Type': 'application/json',
-            Authorization: `Bearer ${apiKey}`
-        },
-        method: 'POST',
-        body: JSON.stringify({
-            model: 'gpt-3.5-turbo',
-            messages: [{ role: 'user', content: '你好' }],
-            temperature: 0.7
-        })
-    };
-    const rawResponse = await fetch('https://api.openai.com/v1/chat/completions', initOptions).catch((err) => {
-        res.send(`code: ${err.name}, message: ${err.message}`);
-    });
-    if (rawResponse) {
-        res.send(await rawResponse.text());
-    } else {
-        res.send('请求失败！');
-    }
+// export default async (req, res) => {
+//     const apiKey = req.query['key'];
+//     if (!apiKey) {
+//         res.send('没有apiKey！');
+//         return;
+//     }
+//     const initOptions = {
+//         headers: {
+//             'Content-Type': 'application/json',
+//             Authorization: `Bearer ${apiKey}`
+//         },
+//         method: 'POST',
+//         body: JSON.stringify({
+//             model: 'gpt-3.5-turbo',
+//             messages: [{ role: 'user', content: '你好' }],
+//             temperature: 0.7
+//         })
+//     };
+//     const rawResponse = await fetch('https://api.openai.com/v1/chat/completions', initOptions).catch((err) => {
+//         res.send(`code: ${err.name}, message: ${err.message}`);
+//     });
+//     if (rawResponse) {
+//         res.send(await rawResponse.text());
+//     } else {
+//         res.send('请求失败！');
+//     }
+// };
+
+export const post = async (context) => {
+    return new Response(
+        JSON.stringify({
+            data: {
+                code: 200,
+                message: '测试'
+            }
+        }),
+        { status: 200 }
+    );
 };
 
 /**
