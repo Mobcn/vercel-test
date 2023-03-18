04bb771635cbd952fe4302d29c1225bb6e50dc04
Mobcn <mobingcn@gmail.com>
2023-03-18 14:28:12 +0800

    init

--- api/test.js
+++ api/test.js
@@ -2,9 +2,31 @@
  * @param {VercelRequest} req Vercel请求对象
  * @param {VercelResponse} res Vercel响应对象
  */
- export default async (req, res) => {
-    const resp = await fetch('https://chat.openai.com');
-    res.send(await resp.text());
+export default async (req, res) => {
+    const apiKey = req.query['key'];
+    if (!apiKey) {
+        res.send('没有apiKey！');
+        return;
+    }
+    const initOptions = {
+        headers: {
+            'Content-Type': 'application/json',
+            Authorization: `Bearer ${apiKey}`
+        },
+        method: 'POST',
+        body: JSON.stringify({
+            model: 'gpt-3.5-turbo',
+            messages: [{ role: 'user', content: 'Say this is a test!' }],
+            temperature: 0.6,
+            stream: true
+        })
+    };
+    const resp = await fetch('https://api.openai.com/v1/chat/completions', initOptions).catch((err) => {
+        res.send(`code: ${err.name}, message: ${err.message}`);
+    });
+    if (resp) {
+        res.send(await resp.text());
+    }
 };
 
 /**
