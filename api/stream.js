9f157cc84219d78426f6f70666a7726b5f5bed45
Mobcn <mobingcn@gmail.com>
2023-03-20 14:00:14 +0800

    test

--- api/stream.js
+++ api/stream.js
@@ -18,26 +18,26 @@
 
 export const config = {
     runtime: 'edge',
-    regions: [
-        'arn1',
-        'bom1',
-        'bru1',
-        'cdg1',
-        'cle1',
-        'cpt1a',
-        'dub1',
-        'fra1',
-        'gru1',
-        'hnd1',
-        'iad1',
-        'icn1',
-        'kix1',
-        'lhr1',
-        'pdx1',
-        'sfo1',
-        'sin1',
-        'syd1'
-    ]
+    // regions: [
+    //     'arn1',
+    //     'bom1',
+    //     'bru1',
+    //     'cdg1',
+    //     'cle1',
+    //     'cpt1a',
+    //     'dub1',
+    //     'fra1',
+    //     'gru1',
+    //     'hnd1',
+    //     'iad1',
+    //     'icn1',
+    //     'kix1',
+    //     'lhr1',
+    //     'pdx1',
+    //     'sfo1',
+    //     'sin1',
+    //     'syd1'
+    // ]
 };
 
 export const post = (context) => {
